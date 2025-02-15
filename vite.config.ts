66fe0acd4c14b47ef0b9bb1954aab1d222ef32ec
sleepyknife <sleepycat7912@gmail.com>
2025-02-15 18:36:20 +0800

    restore to previous vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/tyntrpg_form_vue/' : '/';
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     plugins: [vue()],
-    base
+    base: env.VITE_APP_BASE_URL || '/'
   };
 });
