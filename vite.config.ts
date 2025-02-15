503946b740b002090e0ecb8ddbbc2a1d602edd89
sleepyknife <sleepycat7912@gmail.com>
2025-02-15 18:00:27 +0800

    add vercel production modify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/tyntrpg_form/' : '/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-})
+  return {
+    plugins: [vue()],
+    base: env.VITE_APP_BASE_URL || '/'
+  };
+});
