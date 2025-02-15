b163c16be8767871fffb0b68b36d76f58a9f7280
sleepyknife <sleepycat7912@gmail.com>
2025-02-15 17:43:10 +0800

    add vercel base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tyntrpg_form_vue/' // 例如你的 repo 是 "my-vue-app"，這裡就要填 "/my-vue-app/"
+  base: process.env.NODE_ENV === 'production' ? '/tyntrpg_form/' : '/'
 
 })
